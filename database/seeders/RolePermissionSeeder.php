c3fe60aed252c96b77581f1237e50b89a17b95a2
Al-Mahdi Utshob <mahdiutshob@gmail.com>
2023-06-01 00:22:56 +0600

    Add Role Permission Seeder

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Roles
+        $roleSuperAdmin = Role::create(['name' => 'superadmin']);
+
+
+        // Permission List as array
+        $permissions = [
+
+            // Dashboard
+            'dashboard.view',
+
+            // Blog Permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            // Admin Permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            // Role Permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            // Profile Permissions
+            'profile.view',
+            'profile.edit'
+        ];
+
+
+        // Create and Assign Permissions
+        //
+        for ($i = 0; $i < count($permissions); $i++) {
+            // Create Permission
+            $permission = Permission::create(['name' => $permissions[$i]]);
+            $roleSuperAdmin->givePermissionTo($permission);
+            $permission->assignRole($roleSuperAdmin);
+        }
+    }
+}
